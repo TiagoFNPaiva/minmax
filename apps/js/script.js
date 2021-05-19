a61653af11bade04d13d7ab99f19f8456c82c257
Tiago Paiva <22tiagopaiva22@gmail.com>
2021-05-19 19:03:17 +0100

    estrutura

--- apps/js/script.js
+++ apps/js/script.js
@@ -3,7 +3,7 @@
 const item = document.getElementsByTagName("html")[0]
 
 window.addEventListener('wheel', function (e) {
-    console.log(item.scrollLeft)
+    //console.log(item.scrollLeft)
 
     if (e.deltaY > 0) item.scrollLeft += 400;
     else item.scrollLeft -= 400;
